5bb6ea32d92a3d8680c3c9f988de79b80396a8e8
rezoanaafrinn <rezoanaafrinn@gmail.com>
2025-05-15 16:56:37 +0600

    calculate n factorial

---
+++ factorialN.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int factorialN(int n){
+    int fact = 1;
+    for(int i=1; i<=n; i++){
+        fact *= i;
+    }
+    return fact;
+}
+
+int main(){
+    cout << factorialN(4) << endl;
+    cout << factorialN(6) << endl;
+    return 0;
+}
