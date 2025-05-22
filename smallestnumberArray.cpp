87c7fdca89cdcdf44a4b2401dbe7b409b005f700
rezoanaafrinn <rezoanaafrinn@gmail.com>
2025-05-22 15:48:29 +0600

    find smallest and largest number using array

---
+++ smallestnumberArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int nums[]={5,6,9,10,-12,24};
+    int size = 6;
+
+    int smallest = INT8_MAX;
+    int largest = INT8_MIN;
+    int i= 0;
+
+    for(int i=0; i<=size; i++){
+        // if(nums[i] < smallest){
+        //     smallest = nums[i];
+        // }
+        smallest = min(nums[i],smallest);
+        largest = max(nums[i],largest);
+
+    }
+    cout << "Smallest =" << smallest << endl;
+    cout << "Largest =" << largest << endl;
+    
+    return 0;
+}
