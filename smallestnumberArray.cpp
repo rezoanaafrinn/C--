d25be090f3c2acc82d8c1ce6645f1290571013a6
rezoanaafrinn <rezoanaafrinn@gmail.com>
2025-05-22 16:57:32 +0600

    find smallest and largest number index using array

--- smallestnumberArray.cpp
+++ smallestnumberArray.cpp
@@ -7,18 +7,30 @@ int main(){
 
     int smallest = INT8_MAX;
     int largest = INT8_MIN;
-    int i= 0;
+    int smallest_index;
+    int largest_index;
 
-    for(int i=0; i<=size; i++){
+    for(int i=0; i<size; i++){
         // if(nums[i] < smallest){
         //     smallest = nums[i];
         // }
         smallest = min(nums[i],smallest);
         largest = max(nums[i],largest);
 
+        if(smallest == nums[i] ){
+            smallest_index = i;
+        }
+
+        if(largest == nums[i]){
+            largest_index = i;
+        }
+
     }
-    cout << "Smallest =" << smallest << endl;
-    cout << "Largest =" << largest << endl;
+    cout << "Smallest number =" << smallest << endl;
+    cout << "Smallest Index =" << smallest_index << endl;
+    cout << "Largest number =" << largest << endl;
+    cout << "Largest Index =" << largest_index << endl;
+
     
     return 0;
 }
