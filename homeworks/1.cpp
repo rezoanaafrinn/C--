346d39739023bdfb84217ba47502713ea3684754
rezoanaafrinn <rezoanaafrinn@gmail.com>
2025-05-27 16:34:54 +0600

    sum of all numbers in array

--- homeworks/1.cpp
+++ homeworks/1.cpp
@@ -1,1 +1,12 @@
 // Write a function to calculate sum & product of all numbers in array
+#include<iostream>
+using namespace std;
+void calSum(int arr, int size){
+    
+}
+int main(){
+    int arr[]={1,2,3,4,5,6};
+    int size = 6;
+
+    return 0;
+}
