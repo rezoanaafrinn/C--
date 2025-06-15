3db32be044eabb1b90cd765772c0159b47d96740
rezoanaafrinn <rezoanaafrinn@gmail.com>
2025-06-15 16:38:20 +0600

    vector functions

--- vector_1.cpp
+++ vector_1.cpp
@@ -3,7 +3,20 @@
 using namespace std;
 
 int main() {
-    vector<int> vec = {1,2,3};
-    cout << vec[0] << endl;
+    vector<int> vec;
+
+    cout<< "size = " << vec.size() << endl;
+    vec.push_back(25);
+    vec.push_back(35);
+    vec.push_back(45);
+    vec.push_back(37);
+    vec.push_back(40);
+    cout<< "size after push back= " << vec.size() << endl;
+    vec.pop_back();
+    cout<< "size after pop back= " << vec.size() << endl;
+    cout<< "capacity = " << vec.capacity() << endl;
+    for(int i : vec){
+        cout << i << endl;
+    }
     return 0;
 }
