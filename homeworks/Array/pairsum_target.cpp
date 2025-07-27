0850e3afaf57719ca0140a7f11dc9d1b86c59a3a
rezoanaafrinn <rezoanaafrinn@gmail.com>
2025-07-27 14:39:28 +0600

    pair sum with sort

--- homeworks/Array/pairsum_target.cpp
+++ homeworks/Array/pairsum_target.cpp
@@ -1,33 +1,71 @@
 // return pair in sorted array with target sum
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
-vector<int> pairSum(vector<int> nums,int target){
-    vector<int>ans;
-    int n = nums.size();
+// vector<int> pairSum(vector<int> nums,int target){
+//     vector<int>ans;
+//     int n = nums.size();
 
-    int i = 0, j = n-1;
+//     int i = 0, j = n-1;
 
-    while(i<j){
-        int pairSum = nums[i] + nums[j];
-        if(pairSum > target){
-            j--;
-        }else if(pairSum < target){
+//     while(i<j){
+//         int pairSum = nums[i] + nums[j];
+//         if(pairSum > target){
+//             j--;
+//         }else if(pairSum < target){
+//             i++;
+//         }else {
+//             ans.push_back(i);
+//             ans.push_back(j);
+//             return ans;
+//         }
+//     }
+//     return ans;
+// }
+// int main(){
+//     vector<int> nums = {3,2,4};
+//     int target =6;
+
+//     vector<int> ans = pairSum(nums,target);
+//     cout << ans[0] <<"," << ans[1] <<endl;
+//     return 0;
+// }
+
+vector<int> pairSum(vector<int> nums, int target) {
+    vector<pair<int, int>> numWithIndex;
+    for(int i = 0; i < nums.size(); i++) {
+        numWithIndex.push_back({nums[i], i});
+    }
+
+    // Sort by value
+    sort(numWithIndex.begin(), numWithIndex.end());
+
+    int i = 0, j = nums.size() - 1;
+    while(i < j) {
+        int sum = numWithIndex[i].first + numWithIndex[j].first;
+        if(sum == target) {
+            return {numWithIndex[i].second, numWithIndex[j].second};
+        } else if(sum < target) {
             i++;
-        }else {
-            ans.push_back(i);
-            ans.push_back(j);
-            return ans;
+        } else {
+            j--;
         }
     }
-    return ans;
-}
-int main(){
-    vector<int> nums = {2,7,11,23};
-    int target =13;
 
-    vector<int> ans = pairSum(nums,target);
-    cout << ans[0] <<"," << ans[1] <<endl;
+    return {}; // return empty vector if not found
+}
+
+int main() {
+    vector<int> nums = {3, 2, 4};
+    int target = 6;
+
+    vector<int> ans = pairSum(nums, target);
+    if(!ans.empty())
+        cout << ans[0] << "," << ans[1] << endl;
+    else
+        cout << "No valid pair found" << endl;
+
     return 0;
 }
